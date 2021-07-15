6fc69b3e223761104fd91363ea28d71496755a6d
Gregory Feijon <gregory.feijon@outlook.com>
2021-07-15 18:10:38 -0300

    First commit

---
+++ app/Controllers/Http/UsersController.ts
@@ -0,0 +1,21 @@
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext';
+import User from 'App/Models/User';
+
+export default class UsersController {
+  public async create({ request }: HttpContextContract) {
+    const { name, username } = request.only(['name', 'username']);
+
+    const user = await User.create({
+      name,
+      username,
+    });
+
+    return user;
+  }
+
+  public async getAll() {
+    const all = await User.all();
+
+    return all;
+  }
+}
